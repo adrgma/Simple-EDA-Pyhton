da54fc0288cf956b61482233d38d8ed1923250f8
adrgma <arga.aditia@gmail.com>
2021-09-24 05:44:51 +0700

    update eda2

---
+++ eda2.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import io
+import pandas_profiling
+
+df_retail = pd.read_csv('retail_raw_reduced_data_quality.csv')
+#print(df_retail.head())
+#print(df_retail.dtypes)
+
+length_city = len(df_retail['city'])
+#print('Length kolom city:', length_city)
+
+count_city = df_retail['city'].count()
+#print('Count kolom count_city:', count_city)
+
+number_of_missing_values_city = length_city - count_city
+float_of_missing_values_city = float(number_of_missing_values_city/length_city)
+pct_of_missing_values_city = '{0:.1f}%'.format(float_of_missing_values_city * 100)
+#print('Persentase missing value kolom city:', pct_of_missing_values_city)
+
+
+#print('Kolom quantity')
+#print('Minimum value: ', df_retail['quantity'].min())
+#print('Maximum value: ', df_retail['quantity'].max())
+#print('Mean value: ', df_retail['quantity'].mean())
+#print('Mode value: ', df_retail['quantity'].mode())
+#print('Median value: ', df_retail['quantity'].median())
+#print('Standard Deviation value: ', df_retail['quantity'].std())
+#print(df_retail['quantity'].quantile([0.25, 0.5, 0.75]))
+
+#print('Korelasi quantity dengan item_price')
+#print(df_retail[['quantity', 'item_price']].corr())
+
+report = pandas_profiling.ProfileReport(df_retail)
+report.to_file(output_file = 'report.html')
